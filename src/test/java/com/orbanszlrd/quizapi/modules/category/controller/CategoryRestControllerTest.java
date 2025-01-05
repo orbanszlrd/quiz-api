50426d2df275a23d789e0e3e726f8bb05f4a8c22
Szilard Orban <orbanszlrd@gmail.com>
2025-01-05 23:08:44 +0100

    Fix LocalServerPort import path

--- src/test/java/com/orbanszlrd/quizapi/modules/category/controller/CategoryRestControllerTest.java
+++ src/test/java/com/orbanszlrd/quizapi/modules/category/controller/CategoryRestControllerTest.java
@@ -7,10 +7,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CategoryRestControllerTest {
--- src/test/java/com/orbanszlrd/quizapi/modules/user/controller/UserRestControllerTest.java
+++ src/test/java/com/orbanszlrd/quizapi/modules/user/controller/UserRestControllerTest.java
@@ -12,7 +12,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.*;
